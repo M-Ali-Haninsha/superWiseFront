02f5317fd779ae5ad22a5b4eb93af8648a0ece74
M-Ali-Haninsha <alianinsha@gmail.com>
2023-09-15 19:14:01 +0530

    environment Changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    ADMIN_URL: 'https://superwise.online/admin/',
+    USER_URL: 'https://superwise.online/',
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    ADMIN_URL: 'https://superwise.online/admin/',
+    USER_URL: 'https://superwise.online/',
+  };
