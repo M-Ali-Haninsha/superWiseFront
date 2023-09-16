8d038aec58da06b08d24f50259353e7a7ea20374
M-Ali-Haninsha <alianinsha@gmail.com>
2023-09-16 08:29:24 +0530

    confirmAlert

--- src/app/adminComponents/users/users.component.ts
+++ src/app/adminComponents/users/users.component.ts
@@ -3,6 +3,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { AdminServiceService } from 'src/app/services/admin-service.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-users',
@@ -34,26 +35,60 @@ export class UsersComponent implements OnInit{
     })
   }
 
-  block(data:any) {
+  block(data: any) {
     const requestData = { id: data };
     console.log(requestData);
-    this.service.blockUser(requestData).subscribe((value)=>{
-      console.log(value);
-      if(value == 'done'){
-        this.getUsers()
+  
+    Swal.fire({
+      title: 'Confirm Block',
+      text: 'Are you sure you want to block this user?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, block it!',
+      cancelButtonText: 'No, cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.blockUser(requestData).subscribe((value) => {
+          console.log(value);
+          if (value === 'done') {
+            this.getUsers();
+            Swal.fire('Success', 'User has been blocked!', 'success');
+          } else {
+            Swal.fire('Error', 'Failed to block user', 'error');
+          }
+        });
+      } else {
+        Swal.fire('Canceled', 'The user was not blocked.', 'info');
       }
-    })
+    });
   }
-
-  unBlock(data:any) {
+  
+  unBlock(data: any) {
     const requestData = { id: data };
     console.log(requestData);
-    this.service.unBlockUser(requestData).subscribe((value)=>{
-      console.log(value);
-      if(value == 'done'){
-        this.getUsers()
+  
+    Swal.fire({
+      title: 'Confirm Unblock',
+      text: 'Are you sure you want to unblock this user?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, unblock it!',
+      cancelButtonText: 'No, cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.unBlockUser(requestData).subscribe((value) => {
+          console.log(value);
+          if (value === 'done') {
+            this.getUsers();
+            Swal.fire('Success', 'User has been unblocked!', 'success');
+          } else {
+            Swal.fire('Error', 'Failed to unblock user', 'error');
+          }
+        });
+      } else {
+        Swal.fire('Canceled', 'The user was not unblocked.', 'info');
       }
-    })
+    });
   }
 
   applyFilter(event: Event) {
--- src/app/adminComponents/verified-workers/verified-workers.component.ts
+++ src/app/adminComponents/verified-workers/verified-workers.component.ts
@@ -2,62 +2,103 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-import  { workerData }  from '../../model/workeModel'
+import { workerData } from '../../model/workeModel';
 import { AdminServiceService } from 'src/app/services/admin-service.service';
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-verified-workers',
   templateUrl: './verified-workers.component.html',
-  styleUrls: ['./verified-workers.component.css']
+  styleUrls: ['./verified-workers.component.css'],
 })
-export class VerifiedWorkersComponent implements OnInit{
-  displayedColumns: string[] = ['rowNumber', 'firstName', 'lastName', 'email', 'category','action'];
+export class VerifiedWorkersComponent implements OnInit {
+  displayedColumns: string[] = [
+    'rowNumber',
+    'firstName',
+    'lastName',
+    'email',
+    'category',
+    'action',
+  ];
   dataSource: MatTableDataSource<workerData>;
-  delete: boolean  = false
+  delete: boolean = false;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-
   constructor(private service: AdminServiceService, private route: Router) {
-    this.dataSource = new MatTableDataSource<workerData>;
+    this.dataSource = new MatTableDataSource<workerData>();
   }
 
   ngOnInit(): void {
-      this.getWorker()
+    this.getWorker();
   }
 
   getWorker() {
-    this.service.getVerifiedWorkers().subscribe((value)=>{    
+    this.service.getVerifiedWorkers().subscribe((value) => {
       console.log(value);
-            
-      this.dataSource = new MatTableDataSource(value.workers)
+
+      this.dataSource = new MatTableDataSource(value.workers);
       this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort
-    })
+      this.dataSource.sort = this.sort;
+    });
   }
 
-  block(data:any) {
+  block(data: any) {
     const requestData = { id: data };
     console.log(requestData);
-    this.service.blockWorker(requestData).subscribe((value)=>{
-      console.log(value);
-      if(value == 'done'){
-        this.getWorker()
+
+    Swal.fire({
+      title: 'Confirm Block',
+      text: 'Are you sure you want to block this worker?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, block it!',
+      cancelButtonText: 'No, cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.blockWorker(requestData).subscribe((value) => {
+          console.log(value);
+          if (value === 'done') {
+            this.getWorker();
+            Swal.fire('Success', 'Worker has been blocked!', 'success');
+          } else {
+            Swal.fire('Error', 'Failed to block worker', 'error');
+          }
+        });
+      } else {
+        Swal.fire('Canceled', 'The worker was not blocked.', 'info');
       }
-    })
+    });
   }
 
-  unBlock(data:any) {
+  unBlock(data: any) {
     const requestData = { id: data };
     console.log(requestData);
-    this.service.unBlockWorker(requestData).subscribe((value)=>{
-      console.log(value);
-      if(value == 'done'){
-        this.getWorker()
+
+    Swal.fire({
+      title: 'Confirm Unblock',
+      text: 'Are you sure you want to unblock this worker?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, unblock it!',
+      cancelButtonText: 'No, cancel',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.unBlockWorker(requestData).subscribe((value) => {
+          console.log(value);
+          if (value === 'done') {
+            this.getWorker();
+            Swal.fire('Success', 'Worker has been unblocked!', 'success');
+          } else {
+            Swal.fire('Error', 'Failed to unblock worker', 'error');
+          }
+        });
+      } else {
+        Swal.fire('Canceled', 'The worker was not unblocked.', 'info');
       }
-    })
+    });
   }
 
   applyFilter(event: Event) {
