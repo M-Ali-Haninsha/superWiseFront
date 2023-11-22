9b5a19880e30c8e2f271cd7e75989c04942c2437
M-Ali-Haninsha <alianinsha@gmail.com>
2023-11-22 17:08:37 +0530

    urlChange2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     ADMIN_URL: 'https://superwise-back.onrender.com/admin/',
-    USER_URL: 'https://superwise-back.onrender.com',
+    USER_URL: 'https://superwise-back.onrender.com/',
   };
